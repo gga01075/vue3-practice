a39ef9dec1ce2cfc3f323bcb04f4b5dde57f63ab
gga01075 <gga01075@naver.com>
2023-01-15 15:06:22 +0900

    class,style binding

---
+++ src/lectures/section8/App_class_binding.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div :class="classObject">텍스트 입니다.</div>
+    <button v-on:click="toggle">toggle</button>
+    <button v-on:click="hasError = !hasError">toggle Error</button>
+  </div>
+</template>
+
+<script>
+import { ref, computed } from 'vue';
+export default {
+  setup() {
+    const isActive = ref(true);
+    const hasError = ref(false);
+
+    // const classObject = reactive({
+    //   active: true,
+    //   'text-danger': true,
+    // });
+
+    const classObject = computed(() => {
+      return {
+        active: true,
+        'text-danger': true,
+      };
+    });
+
+    const toggle = () => {
+      isActive.value = !isActive.value;
+    };
+
+    return { isActive, toggle, hasError, classObject };
+  },
+};
+</script>
+
+<style scoped>
+.active {
+  font-weight: 900;
+}
+
+.text-danger {
+  color: red;
+}
+</style>
---
+++ src/lectures/section8/App_style_binding.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div :style="styleObject">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates,
+      laudantium mollitia! Et delectus excepturi vero laudantium, sunt fuga
+      optio, vel laboriosam, fugit obcaecati quaerat eius ipsam. Libero esse
+      facilis eaque!
+    </div>
+    <button v-on:click="fontSize--">-</button>
+    <button v-on:click="fontSize++">+</button>
+  </div>
+</template>
+
+<script>
+import { ref, computed } from 'vue';
+export default {
+  setup() {
+    // const styleObject = reactive({
+    //   color: 'red',
+    //   fontSize: '13px',
+    // });
+    const fontSize = ref(13);
+    const styleObject = computed(() => {
+      return {
+        color: 'red',
+        fontSize: fontSize.value + 'px',
+      };
+    });
+
+    return { fontSize, styleObject };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
