d711e10261f7f654f64bc5f2aecc2a9a93ad6430
gga01075 <gga01075@naver.com>
2023-01-15 14:40:52 +0900

    computed

---
+++ src/lectures/section7/App_computed.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+		<h2>{{ teacher.name }}</h2>
+		<h3>강의가 있습니까??</h3>
+		<p>{{ hasLecture }}</p>
+		<p>{{ hasLecture }}</p>
+		<p>{{ hasLecture }}</p>
+		<p>{{ existLecture() }}</p>
+		<p>{{ existLecture() }}</p>
+		<p>{{ existLecture() }}</p>
+	</div>
+</template>
+
+<script>
+import { reactive, computed } from 'vue';
+
+export default {
+	setup() {
+		const teacher = reactive({
+			name: 'HSH',
+			lectures: ['HTML/CSS', 'JavaScript', 'Vue3'],
+		});
+
+		const hasLecture = computed(() => {
+			console.log('computed');
+			return teacher.lectures.length > 0 ? '있음 😀' : '없음 😅';
+		});
+
+		const existLecture = () => {
+			console.log('method');
+			return teacher.lectures.length > 0 ? '있음 😀' : '없음 😅';
+		};
+
+		return { teacher, hasLecture, existLecture };
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/lectures/section7/App_computed_getter_setter.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>이름</h2>
+    <p>{{ fullName }}</p>
+  </div>
+</template>
+
+<script>
+import { ref, computed } from 'vue';
+
+export default {
+  setup() {
+    const firstName = ref('홍');
+    const lastName = ref('길동');
+
+    const fullName = computed({
+      get() {
+        return firstName.value + ' ' + lastName.value;
+      },
+      set(value) {
+        [firstName.value, lastName.value] = value.split(' ');
+      },
+    });
+
+    fullName.value = '짐 코딩';
+
+    return { fullName };
+  },
+};
+</script>
