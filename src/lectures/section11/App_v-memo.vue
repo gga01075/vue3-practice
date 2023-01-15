c0cae624926c73a3e0f4b26426eb53f485025577
gga01075 <gga01075@naver.com>
2023-01-15 17:25:13 +0900

    v-memo

---
+++ src/lectures/section11/App_v-memo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div v-memo="[subscriber]">
+      <p>subscriber : {{ subscriber }}</p>
+      <p>views : {{ views }}</p>
+      <p>likes : {{ likes }}</p>
+    </div>
+    <button @click="subscriber++">Subs++</button>
+    <button @click="views++">Views++</button>
+    <button @click="likes++">Likes++</button>
+
+    <div>
+      <p>subscriber : {{ subscriber }}</p>
+      <p>views : {{ views }}</p>
+      <p>likes : {{ likes }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+  setup() {
+    const subscriber = ref(4000);
+    const views = ref(400);
+    const likes = ref(20);
+    return { subscriber, views, likes };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
